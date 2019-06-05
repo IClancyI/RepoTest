7ce8a0627d48e2a2f7542fcdca1cdc057d44fd28
Jaric De Becker <jaric.debecker@gmail.com>
2019-06-05 21:49:02 +0200

    Console writeline toegevoegd aan project.

--- ditIsEenConsoleTest/ditIsEenConsoleTest/Program.cs
+++ ditIsEenConsoleTest/ditIsEenConsoleTest/Program.cs
@@ -10,6 +10,8 @@ namespace ditIsEenConsoleTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Dit is een test.");
+            Console.ReadLine();
         }
     }
 }
